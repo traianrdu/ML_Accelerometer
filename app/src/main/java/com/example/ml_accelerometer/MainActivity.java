8ce7dd7837ffd7798a878d13822d16aa4beac3a0
Traian <traian.plosca@gmail.com>
2020-10-08 14:06:25 +0300

    commit save to drive csv file

--- app/src/main/java/com/example/ml_accelerometer/MainActivity.java
+++ app/src/main/java/com/example/ml_accelerometer/MainActivity.java
@@ -1,9 +1,16 @@
 package com.example.ml_accelerometer;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.FileProvider;
 
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
+
+import java.io.File;
+import java.io.FileOutputStream;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,6 +21,31 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void export(View view){
+        //Generate data
+        StringBuilder data= new StringBuilder();
+        data.append("Time,Distance");
+        for(int i=0; i<5; i++)
+            data.append("\n"+String.valueOf(i)+","+String.valueOf(i*i));
+        try {
+            //saving the file into service
+            FileOutputStream out = openFileOutput("data.csv", Context.MODE_PRIVATE);
+            out.write((data.toString()).getBytes());
+            out.close();
+
+            //exporting
+            Context context = getApplicationContext();
+            File filelocation = new File(getFilesDir(), "data.csv");
+            Uri path = FileProvider.getUriForFile(context, "com.example.ML_Accelerometer.fileprovider",filelocation);
+            Intent fileIntent = new Intent(Intent.ACTION_SEND);
+            fileIntent.setType("text/csv");
+            fileIntent.putExtra(Intent.EXTRA_SUBJECT, "Data");
+            fileIntent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
+            fileIntent.putExtra(Intent.EXTRA_STREAM, path);
+            startActivity(Intent.createChooser(fileIntent, "Send mail"));
+
+        }catch (Exception e){
+            e.printStackTrace();
+        }
 
     }
 }
