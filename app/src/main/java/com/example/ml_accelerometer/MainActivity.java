ca3e78def06ccc9afa4dd4a8a717e79d5f25d7a2
Traian <traian.plosca@gmail.com>
2020-10-14 13:16:54 +0300

    commit accelerometer

--- app/src/main/java/com/example/ml_accelerometer/MainActivity.java
+++ app/src/main/java/com/example/ml_accelerometer/MainActivity.java
@@ -5,19 +5,37 @@ import androidx.core.content.FileProvider;
 
 import android.content.Context;
 import android.content.Intent;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
 import java.io.File;
 import java.io.FileOutputStream;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements SensorEventListener {
+
+    private SensorManager sensorManager;
+    Sensor accelerometer;
+
+    TextView xValue, yValue, zValue;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        xValue = (TextView) findViewById(R.id.xValue);
+
+        sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        accelerometer = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        sensorManager.registerListener(MainActivity.this, accelerometer, SensorManager.SENSOR_DELAY_NORMAL);
+
     }
 
     public void export(View view){
@@ -25,7 +43,7 @@ public class MainActivity extends AppCompatActivity {
         StringBuilder data= new StringBuilder();
         data.append("Time,Distance");
         for(int i=0; i<5; i++)
-            data.append("\n"+String.valueOf(i)+","+String.valueOf(i*i));
+            data.append("\n").append(String.valueOf(i)).append(",").append(String.valueOf(i * i));
         try {
             //saving the file into service
             FileOutputStream out = openFileOutput("data.csv", Context.MODE_PRIVATE);
@@ -46,6 +64,16 @@ public class MainActivity extends AppCompatActivity {
         }catch (Exception e){
             e.printStackTrace();
         }
+
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent sensorEvent) {
+
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int i) {
 
     }
 }
